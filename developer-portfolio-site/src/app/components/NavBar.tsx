6e51405739ec4e3278b4040375f4309e17e8b27d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-08 17:57:24 +0100

    add state management for opening and closing sidebar menu, add event listeners on menu and close menu icons to change state

--- developer-portfolio-site/src/app/components/NavBar.tsx
+++ developer-portfolio-site/src/app/components/NavBar.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 
 const NavBar = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [openMenu, setOpenMenu] = useState(true);
 
   return (
     <nav className="w-full h024 shadow-xl bg-white">
@@ -44,50 +45,62 @@ const NavBar = () => {
         )}
 
         {/* Mobile Menu */}
-        <div className="sm:hidden cursor-pointer">
-          <RiMenu3Fill className="h-8 w-8 text-orange-500" />
+        <div
+          //   onClick={() => setOpenMenu(false)}
+          className="sm:hidden cursor-pointer"
+        >
+          <RiMenu3Fill
+            onClick={() => setOpenMenu(!openMenu)}
+            size={40}
+            className=" text-orange-500"
+          />
         </div>
       </div>
-      <div className="fixed top-0 left-0 w-[75%] sm:hidden h-screen bg-orange-500 p-10 ease-in-out duration-500">
-        <div className="flex w-full items-center justify-end">
-          <div className="flex justify-between w-full cursor-pointer mb-8">
-            {darkMode ? (
-              <MdDarkMode size={30} className="text-white" />
-            ) : (
-              <div>
-                <MdLightMode size={30} className="text-white" />
-              </div>
-            )}
-            <RiCloseFill className="h-8 w-8 text-white" />
+      {openMenu && (
+        <div className="fixed top-0 left-0 w-[75%] sm:hidden h-screen bg-orange-500 p-10 ease-in-out duration-500">
+          <div className="flex w-full items-center justify-end">
+            <div className="flex justify-between w-full cursor-pointer mb-8">
+              {darkMode ? (
+                <MdDarkMode size={30} className="text-white" />
+              ) : (
+                <div>
+                  <MdLightMode size={30} className="text-white" />
+                </div>
+              )}
+              <RiCloseFill
+                onClick={() => setOpenMenu(false)}
+                className="h-8 w-8 text-white"
+              />
+            </div>
+          </div>
+          {/* Mobile Menu Links */}
+          <div className="flex flex-col">
+            <ul className="flex flex-col gap-2">
+              {NAV_LINKS.map((link) => (
+                <li className="nav-links-mobile" key={link.key}>
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Social media links */}
+
+          <div className="flex flex-row justify-around items-center mt-10">
+            <a target="_blank" href={process.env.GITHUB_LINK}>
+              <BsGithub
+                size={30}
+                className="text-white hover:text-black cursor-pointer ease-in-out duration-300"
+              />
+            </a>
+            <a target="_blank" href={process.env.LINKEDIN_LINK}>
+              <BsLinkedin
+                size={30}
+                className="text-white hover:text-black cursor-pointer ease-in-out duration-300"
+              />
+            </a>
           </div>
         </div>
-        {/* Mobile Menu Links */}
-        <div className="flex flex-col">
-          <ul className="flex flex-col gap-2">
-            {NAV_LINKS.map((link) => (
-              <li className="nav-links-mobile" key={link.key}>
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* Social media links */}
-
-        <div className="flex flex-row justify-around items-center mt-10">
-          <a target="_blank" href={process.env.GITHUB_LINK}>
-            <BsGithub
-              size={30}
-              className="text-white hover:text-black cursor-pointer ease-in-out duration-300"
-            />
-          </a>
-          <a target="_blank" href={process.env.LINKEDIN_LINK}>
-            <BsLinkedin
-              size={30}
-              className="text-white hover:text-black cursor-pointer ease-in-out duration-300"
-            />
-          </a>
-        </div>
-      </div>
+      )}
     </nav>
   );
 };
