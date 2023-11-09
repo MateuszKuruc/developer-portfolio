05ef872f8964c4d9de0986f9dd991d6c8f118ee7
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-09 13:35:33 +0100

    clean code, add data in constants folder to use for mapping over skills and provide src, name and labels

--- developer-portfolio-site/src/app/components/Skills.tsx
+++ developer-portfolio-site/src/app/components/Skills.tsx
@@ -1,123 +1,25 @@
 import Image from "next/image";
 import React from "react";
 
+import {
+  FRONTEND_SKILLS,
+  BACKEND_SKILLS,
+  OTHER_SKILLS,
+} from "../constants/textConstants";
+
 const Skills = () => {
   return (
-    <section id="skills" className="bg-white flex flex-col items-center dark:bg-blue-950">
+    <section
+      id="skills"
+      className="bg-white flex flex-col items-center dark:bg-blue-950"
+    >
       <h2 className="bold-40 content-container">Skills</h2>
 
       <h3 className="bold-20">Frontend</h3>
       <div className="w-full padding-container border-2 border-blue-400">
-        <ul className="gap-8 grid grid-cols-5">
-          <li>
-            <Image
-              src="/javascript-original.svg"
-              width={70}
-              height={70}
-              alt="javascript icon"
-              className="rounded-2xl text-white bg-white"
-              color="black"
-            />
-            JavaScript
-          </li>
-          <li>
-            <Image
-              src="/react-original.svg"
-              width={70}
-              height={70}
-              alt="react icon"
-              className="rounded-2xl"
-            />
-            React
-          </li>
-          <li>
-            <Image
-              src="/nextjs-original.svg"
-              width={70}
-              height={70}
-              alt="nextjs icon"
-              className="rounded-2xl"
-            />
-            NextJs
-          </li>
-          <li> <Image
-                src="/html5-original.svg"
-                width={70}
-                height={70}
-                alt="react icon"
-                className="rounded-2xl"
-              />HTML</li>
-          <li>
-          <Image
-                src="/css3-original.svg"
-                width={70}
-                height={70}
-                alt="react icon"
-                className="rounded-2xl"
-              />
-              CSS
-          </li>
-          <li></li>
+     
         </ul>
       </div>
-
-
-
-
-      <h3 className="bold-20">Backend</h3>
-      <div className="w-full padding-container">
-        <ul className="flex gap-8">
-          <li>
-            <Image
-              src="/nodejs-original.svg"
-              width={70}
-              height={70}
-              alt="javascript icon"
-              className="rounded-2xl"
-            />
-            NodeJS
-          </li>
-          <li>
-            <Image
-              src="/react-original.svg"
-              width={70}
-              height={70}
-              alt="react icon"
-              className="rounded-2xl"
-            />
-            React
-          </li>
-          <li>
-            <Image
-              src="/nextjs-original.svg"
-              width={70}
-              height={70}
-              alt="nextjs icon"
-              className="rounded-2xl"
-            />
-            NextJs
-          </li>
-          <li> <Image
-                src="/html5-original.svg"
-                width={70}
-                height={70}
-                alt="react icon"
-                className="rounded-2xl"
-              />HTML</li>
-          <li>
-          <Image
-                src="/css3-original.svg"
-                width={70}
-                height={70}
-                alt="react icon"
-                className="rounded-2xl"
-              />
-              CSS
-          </li>
-          <li></li>
-        </ul>
-      </div>
-      
     </section>
   );
 };
--- developer-portfolio-site/src/app/constants/textConstants.ts
+++ developer-portfolio-site/src/app/constants/textConstants.ts
@@ -5,3 +5,30 @@ export const NAV_LINKS = [
   { href: "#contact", key: "contact", label: "Contact" },
   { href: "/resume", key: "resume", label: "Resume" },
 ];
+
+export const FRONTEND_SKILLS = [
+  { src: "/html.svg", name: "HTML", label: "HTML icon" },
+  { src: "/css.svg", name: "CSS", label: "CSS icon" },
+  { src: "/javascript.svg", name: "JavaScript", label: "JavaScript icon" },
+  { src: "/nextjs.svg", name: "NextJS", label: "NextJS icon" },
+  { src: "/react.svg", name: "React", label: "React icon" },
+  { src: "/jest.svg", name: "jest", label: "jest icon" },
+  { src: "/mui.svg", name: "Material UI", label: "Material UI icon" },
+  { src: "/sass.svg", name: "SASS", label: "SASS icon" },
+  { src: "/tailwind.svg", name: "Tailwind CSS", label: "Tailwind icon" },
+  { src: "/typescript.svg", name: "TypeScript", label: "Typescript icon" },
+];
+
+export const BACKEND_SKILLS = [
+  { src: "/nodejs.svg", name: "NodeJS", label: "NodeJS icon" },
+  { src: "/express.svg", name: "Express", label: "Express icon" },
+  { src: "/aws.svg", name: "AWS S3", label: "AWS icon" },
+  { src: "/mongodb.svg", name: "MongoDB", label: "MongoDB icon" },
+];
+
+export const OTHER_SKILLS = [
+  { src: "/graphql.svg", name: "GraphQL", label: "Graphql icon" },
+  { src: "/jira.svg", name: "Jira", label: "Jira icon" },
+  { src: "/git.svg", name: "Git", label: "Git icon" },
+  { src: "/github.svg", name: "Github", label: "Github icon" },
+];
