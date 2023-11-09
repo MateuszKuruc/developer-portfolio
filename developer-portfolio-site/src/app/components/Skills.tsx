2f2d9a1f7a8cd2cd24368dfc13bbd6d96620ee0f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-09 13:11:15 +0100

    add skills component to display icons of tools I am working with

---
+++ developer-portfolio-site/src/app/components/Skills.tsx
@@ -0,0 +1,163 @@
+import Image from "next/image";
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section id="skills" className="bg-white flex flex-col items-center dark:bg-blue-950">
+      <h2 className="bold-40 content-container">Skills</h2>
+
+      <h3 className="bold-20">Frontend</h3>
+      <div className="w-full padding-container border-2 border-blue-400">
+        <ul className="gap-8 grid grid-cols-5">
+          <li>
+            <Image
+              src="/javascript-original.svg"
+              width={70}
+              height={70}
+              alt="javascript icon"
+              className="rounded-2xl text-white bg-white"
+              color="black"
+            />
+            JavaScript
+          </li>
+          <li>
+            <Image
+              src="/react-original.svg"
+              width={70}
+              height={70}
+              alt="react icon"
+              className="rounded-2xl"
+            />
+            React
+          </li>
+          <li>
+            <Image
+              src="/nextjs-original.svg"
+              width={70}
+              height={70}
+              alt="nextjs icon"
+              className="rounded-2xl"
+            />
+            NextJs
+          </li>
+          <li> <Image
+                src="/html5-original.svg"
+                width={70}
+                height={70}
+                alt="react icon"
+                className="rounded-2xl"
+              />HTML</li>
+          <li>
+          <Image
+                src="/css3-original.svg"
+                width={70}
+                height={70}
+                alt="react icon"
+                className="rounded-2xl"
+              />
+              CSS
+          </li>
+          <li></li>
+        </ul>
+      </div>
+
+
+
+
+      <h3 className="bold-20">Backend</h3>
+      <div className="w-full padding-container">
+        <ul className="flex gap-8">
+          <li>
+            <Image
+              src="/nodejs-original.svg"
+              width={70}
+              height={70}
+              alt="javascript icon"
+              className="rounded-2xl"
+            />
+            NodeJS
+          </li>
+          <li>
+            <Image
+              src="/react-original.svg"
+              width={70}
+              height={70}
+              alt="react icon"
+              className="rounded-2xl"
+            />
+            React
+          </li>
+          <li>
+            <Image
+              src="/nextjs-original.svg"
+              width={70}
+              height={70}
+              alt="nextjs icon"
+              className="rounded-2xl"
+            />
+            NextJs
+          </li>
+          <li> <Image
+                src="/html5-original.svg"
+                width={70}
+                height={70}
+                alt="react icon"
+                className="rounded-2xl"
+              />HTML</li>
+          <li>
+          <Image
+                src="/css3-original.svg"
+                width={70}
+                height={70}
+                alt="react icon"
+                className="rounded-2xl"
+              />
+              CSS
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      {/* <div className="w-full padding-container flex justify-around bg-yellow-500">
+        <div>
+          <h3 className="bold-32">Frontend</h3>
+          <ul className="flex gap-8">
+            <li>
+              <Image
+                src="/javascript-original.svg"
+                width={70}
+                height={70}
+                alt="javascript icon"
+                className="rounded-2xl"
+              />
+              JavaScript
+            </li>
+            <li>
+              <Image
+                src="/react-original.svg"
+                width={70}
+                height={70}
+                alt="react icon"
+                className="rounded-2xl"
+              />
+              React
+            </li>
+            <li>NextJs</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="bold-32">Backend</h3>
+          <ul>
+            <li>NodeJs</li>
+            <li>Express</li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="bold-32">Other</h3>
+        </div>
+      </div> */}
+    </section>
+  );
+};
+
+export default Skills;
--- developer-portfolio-site/src/app/page.tsx
+++ developer-portfolio-site/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Skills from "./components/Skills";
+
 export default function Home() {
   return (
-    <div className="h-[90vh]">
-      <h1 className="text-blue-500 bold-52">Home</h1>
-    </div>
+    <>
+      <Skills />
+    </>
   );
 }
