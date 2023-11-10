1cf0ae6262386f53d1ed3d0f3ae73de8ad779e90
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-10 14:20:05 +0100

    style footer for desktop

--- developer-portfolio-site/src/app/components/Footer.tsx
+++ developer-portfolio-site/src/app/components/Footer.tsx
@@ -1,20 +1,44 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+
 const Footer = () => {
   return (
     <section className="content-container">
-      <div>
-        <h4 className="bold-18">Contact</h4>
-        <p className="reguar-18">
-          Email:{" "}
-          <span className="contact-details contact-email">
-            <a href={`mailto:${process.env.CONTACT_EMAIL}`}>
-              {process.env.CONTACT_EMAIL}
-            </a>
-          </span>
-        </p>
-        <p className="regular-18">
-          Phone:{" "}
-          <span className="contact-details">{process.env.CONTACT_NUMBER}</span>
-        </p>
+      <div className="p-8 flex items-center justify-around bg-green-500">
+        <div>
+          <p className="bold-18">Mateusz Kuruc © 2023</p>
+        </div>
+
+        <div>
+          <h4 className="bold-18">Contact</h4>
+          <p className="regular-18">
+            Email:{" "}
+            <span className="contact-details contact-email">
+              <a href={`mailto:${process.env.CONTACT_EMAIL}`}>
+                {process.env.CONTACT_EMAIL}
+              </a>
+            </span>
+          </p>
+          <p className="regular-18">
+            Phone:{" "}
+            <span className="contact-details">
+              {process.env.CONTACT_NUMBER}
+            </span>
+          </p>
+        </div>
+        <div>
+          <h4 className="bold-18 pb-2">My profiles</h4>
+         
+<div className="flex gap-4 justify-center">
+
+          <a target="_blank" href={process.env.LINKEDIN_LINK}>
+            <BsLinkedin size={30} />
+          </a>
+          <a target="_blank" href={process.env.GITHUB_LINK}>
+            <BsGithub size={30} />
+          </a>
+          
+</div>
+        </div>
       </div>
     </section>
   );
