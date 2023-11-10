59ade2dc413471f41c6920e19595e76f017baaf8
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-10 12:48:49 +0100

    add padding container around introductory paragraph and image

--- developer-portfolio-site/src/app/components/About.tsx
+++ developer-portfolio-site/src/app/components/About.tsx
@@ -7,42 +7,31 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 const About = () => {
   return (
     <section className="content-container">
-      {/* <div className="padding-container flex flex-col items-start ml-0"> */}
-        <div className="flex flex-col xl:flex-row items-center justify-around py-10 sm:py-20 2xl:py-40 gap-10">
-          <div className="3xl:max-w-[800px] sm:max-w-[600px]">
-            <h2 className="bold-40 leading-[120%]">Mateusz Kuruc</h2>
-            <h2 className="bold-40 leading-[120%] border-b-8 rounded-md border-orange-500 mb-6 pb-2">
-              Junior JavaScript Developer
-            </h2>
-            <p className="regular-18 leading-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              pellentesque justo magna, non fermentum quam fermentum sit amet.
-              Fusce sit amet scelerisque lorem. Nullam vel euismod mi, vitae
-              ultricies turpis. Integer vitae tortor felis. Aliquam nec velit
-              vel ante maximus rhoncus. Fusce orci sem, hendrerit nec aliquam
-              ac, mattis quis augue.
-            </p>
-          </div>
-          <div>
-            <Image
-              src="/mateusz_kuruc.jpeg"
-              alt="author photo"
-              width={600}
-              height={600}
-              className="rounded-xl"
-            />
-          </div>
+      <div className="padding-container flex flex-col xl:flex-row items-center justify-around py-10 sm:py-20 2xl:py-40 gap-10">
+        <div className="3xl:max-w-[800px] sm:max-w-[600px]">
+          <h2 className="bold-40 leading-[120%]">Mateusz Kuruc</h2>
+          <h2 className="bold-40 leading-[120%] border-b-8 rounded-md border-orange-500 mb-6 pb-2">
+            Junior JavaScript Developer
+          </h2>
+          <p className="regular-18 leading-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            pellentesque justo magna, non fermentum quam fermentum sit amet.
+            Fusce sit amet scelerisque lorem. Nullam vel euismod mi, vitae
+            ultricies turpis. Integer vitae tortor felis. Aliquam nec velit vel
+            ante maximus rhoncus. Fusce orci sem, hendrerit nec aliquam ac,
+            mattis quis augue.
+          </p>
         </div>
-
-        {/* <div>
-          <Link href="#skills">
-            <div className="flex items-center justify-center gap-2">
-              <p>See how I build my stuff</p>
-              <AiOutlineArrowDown size={30} />
-            </div>
-          </Link>
-        </div> */}
-      {/* </div> */}
+        <div>
+          <Image
+            src="/mateusz_kuruc.jpeg"
+            alt="author photo"
+            width={600}
+            height={600}
+            className="rounded-xl"
+          />
+        </div>
+      </div>
     </section>
   );
 };
