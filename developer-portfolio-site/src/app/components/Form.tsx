c9d797789ac456f8625769c68b71e8336a3a1b1a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-10 09:46:25 +0100

    write basis of form component

--- developer-portfolio-site/src/app/components/Form.tsx
+++ developer-portfolio-site/src/app/components/Form.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useFormik } from "formik";
+
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" placeholder="Enter your name" />
+
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" placeholder="Enter your emal" />
+
+      <label htmlFor="name">Message</label>
+      <textarea id="message" placeholder="Write the message..." />
+    </form>
+  );
 };
 
 export default Form;
--- developer-portfolio-site/src/app/page.tsx
+++ developer-portfolio-site/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Skills from "./components/Skills";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
     <>
       <About />
       <Skills />
+      <Contact />
     </>
   );
 }
