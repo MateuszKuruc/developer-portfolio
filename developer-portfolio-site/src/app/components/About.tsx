9c54b7b01d1bb77401658d7fa52093f41a8de26f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-10 07:14:21 +0100

    fix about section styling

--- developer-portfolio-site/src/app/components/About.tsx
+++ developer-portfolio-site/src/app/components/About.tsx
@@ -7,8 +7,8 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 const About = () => {
   return (
     <section className="content-container">
-      <div className="padding-container flex flex-col items-start ml-0">
-        <div className="flex flex-col xl:flex-row items-center py-10 sm:py-20 2xl:py-40 gap-10">
+      {/* <div className="padding-container flex flex-col items-start ml-0"> */}
+        <div className="flex flex-col xl:flex-row items-center justify-around py-10 sm:py-20 2xl:py-40 gap-10">
           <div className="max-w-[800px]">
             <h2 className="bold-40 leading-[120%]">Mateusz Kuruc</h2>
             <h2 className="bold-40 leading-[120%] border-b-8 rounded-md border-orange-500 mb-6 pb-2">
@@ -42,7 +42,7 @@ const About = () => {
             </div>
           </Link>
         </div> */}
-      </div>
+      {/* </div> */}
     </section>
   );
 };
