bdda76e28fc7ba6e92c4086e80b15837405dae15
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-10 06:36:09 +0100

    style about section

--- developer-portfolio-site/src/app/components/About.tsx
+++ developer-portfolio-site/src/app/components/About.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const About = () => {
   return (
     <section className="content-container">
-      <div className="flex items-center justify-around py-40">
-        <div className="max-w-[600px]">
-          <h2 className="bold-40 leading-[150%]">Mateusz Kuruc</h2>
-          <h2 className="bold-40 leading-[150%]">Junior JavaScript Developer</h2>
+      <div className="flex flex-col xl:flex-row items-center justify-around py-10 sm:py-40 gap-10">
+        <div className="max-w-[800px] padding-container">
+          <h2 className="bold-40 leading-[120%]">Mateusz Kuruc</h2>
+          <h2 className="bold-40 leading-[120%] border-b-8 rounded-md border-orange-500 mb-6 pb-2">Junior JavaScript Developer</h2>
           <p className="regular-18 leading-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
             pellentesque justo magna, non fermentum quam fermentum sit amet.
