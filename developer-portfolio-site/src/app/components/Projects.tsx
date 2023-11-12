86e4527bec2498823f3b69e867902b669392ab92
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-12 19:27:12 +0100

    adjust styling for the second project container

--- developer-portfolio-site/src/app/components/Projects.tsx
+++ developer-portfolio-site/src/app/components/Projects.tsx
@@ -74,11 +74,46 @@ const Projects = () => {
             className="absolute top-1/3 md:left-3/4 left-3/4 w-[100px] md:w-[150px]"
           />
         </div>
-        <div className="padding-container">
+        {/* <div className="padding-container">
           <h2 className="bold-40 text-center">Marketing portfolio website</h2>
           <p className="regular-16 border-t-2 border-b-2 py-4">
             Next.js ◆ TypeScript ◆ Tailwind ◆ Formik ◆ Yup
           </p>
+        </div> */}
+        <div className="padding-container flex flex-col justify-center items-center">
+          <h2 className="bold-40 text-center">Marketing Portfolio website</h2>
+          <p className="regular-16 border-t-2 border-b-2 py-4 max-w-[700px]">
+            Next.js ◆ TypeScript ◆ Tailwind ◆ Formik ◆ Yup
+          </p>
+
+          <div className="max-w-[700px]">
+            <p className="regular-18 py-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              efficitur ex sit amet sollicitudin porta.
+            </p>
+            <ul className="list-disc">
+              <li>
+                Nulla quis ex sed dolor iaculis gravida. Curabitur quis turpis
+                diam. Etiam euismod, tellus ut pretium pharetra, sem odio
+                rhoncus ante, sit amet condimentum nunc elit id metus.
+              </li>
+              <li>
+                Donec non felis mi. Donec nec nulla vitae lacus efficitur
+                lobortis. Cras malesuada vehicula neque, in dictum nulla
+                porttitor scelerisque.
+              </li>
+              <li>
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas. Maecenas vitae lectus quis
+                risus ultrices malesuada.
+              </li>
+              <li>
+                Quisque sollicitudin sit amet est et pellentesque. Etiam
+                lobortis massa vitae velit commodo, sit amet convallis massa
+                commodo.{" "}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
