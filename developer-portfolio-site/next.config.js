c45ecfefe6ee0d43654619f331eeab35fa5a7110
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-15 00:11:07 +0100

    update next config file

--- developer-portfolio-site/next.config.js
+++ developer-portfolio-site/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["www.mateuszkuruc.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
