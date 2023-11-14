8ef432d39686e53fd9b46026ec3eee748cec338e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-14 19:17:43 +0100

    add and style summary section to marketing project page

---
+++ developer-portfolio-site/src/app/components/marketing-project-components/MarketingSummary.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-unescaped-entities */
+const MarketingSummary = () => {
+  return (
+    <>
+      <div className="padding-container regular-18 py-8">
+        <h2 className="bold-32-tight dark:text-orange-500 py-8 text-center md:text-left">
+          Summary
+        </h2>
+        <p className="">
+          The marketing website is already deployed and used{" "}
+          <strong>commercially</strong>, you can check this out (especially if
+          you are looking for a marketing specialist). The app looks and works
+          very well, with no complaints from users' side.
+        </p>
+        <p className="pt-4">
+          This project pushed my understanding of modern web development to a
+          new level. These days I am even more aware of notions like
+          <strong>accessibility, performance </strong>or{" "}
+          <strong>responsive design</strong>.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default MarketingSummary;
--- developer-portfolio-site/src/app/projects/marketing-portfolio/page.tsx
+++ developer-portfolio-site/src/app/projects/marketing-portfolio/page.tsx
@@ -1,6 +1,7 @@
 import MarketingChallenges from "@/app/components/marketing-project-components/MarketingChallenges";
 import MarketingHeader from "@/app/components/marketing-project-components/MarketingHeader";
 import MarketingKeyFeatures from "@/app/components/marketing-project-components/MarketingKeyFeatures";
+import MarketingSummary from "@/app/components/marketing-project-components/MarketingSummary";
 import MarketingTechStack from "@/app/components/marketing-project-components/MarketingTechStack";
 
 const MarketingProject = () => {
@@ -10,6 +11,7 @@ const MarketingProject = () => {
       <MarketingKeyFeatures />
       <MarketingTechStack />
       <MarketingChallenges />
+      <MarketingSummary />
     </section>
   );
 };
