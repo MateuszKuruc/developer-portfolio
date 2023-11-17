229ed2a07d8fdfa69cc5021e0bf955492bde84c4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-17 14:57:36 +0100

    add pdf viewer and pdf download to resume page

--- developer-portfolio-site/src/app/resume/page.tsx
+++ developer-portfolio-site/src/app/resume/page.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import Link from "next/link";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const Resume = () => {
   return (
     <section className="content-container">
-      <div>
-        <a href="Kino.pdf" download="Kino.pdf">
-          <button className="btn-submit my-10">Download resume</button>
+      <div className="flex flex-col gap-12">
+        <h2 className="bold-40 pt-12">Resume</h2>
+
+        <p className="regular-18">
+          Please take a look at my resume and feel free to download it, if
+          needed!
+        </p>
+        <a href="/Kino.pdf" download="Kino.pdf">
+          <div className="flex items-end gap-4 justify-center">
+            <h3 className="bold-24 text-transparent bg-clip-text bg-gradient-to-r from-orange-500 via-pink-500 to-blue-700">
+              Download resume
+            </h3>
+            <AiOutlineArrowLeft size={30} className="animate-bounce" />
+          </div>
         </a>
-        <iframe src="/Kino.pdf" width="1000px" height="1000px"></iframe>
+      </div>
+      <div className="flex flex-col items-center py-12">
+        <iframe
+          className="rounded-lg shadow-image"
+          src="/Kino.pdf"
+          width="1000px"
+          height="1000px"
+        ></iframe>
       </div>
     </section>
   );
