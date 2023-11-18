c0e7e53b8dead608743eb3ee6da6c6ef49b1aae6
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-18 12:10:50 +0100

    style resume section

--- developer-portfolio-site/src/app/resume/page.tsx
+++ developer-portfolio-site/src/app/resume/page.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import Link from "next/link";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const Resume = () => {
   return (
     <section className="content-container">
-      <div className="flex flex-col gap-12">
-        <h2 className="bold-40 pt-12">Resume</h2>
+      <div className="padding-container flex flex-col gap-12">
+        <h2 className="bold-40 pt-12 text-center md:text-left">Resume</h2>
 
         <p className="regular-18">
           Please take a look at my resume and feel free to download it, if
           needed!
         </p>
-        <a href="/Kino.pdf" download="Kino.pdf">
+        <a href="/cv.pdf" download="cv.pdf">
           <div className="flex items-end gap-4 justify-center">
             <h3 className="bold-24 hover:scale-105 ease-in-out duration-300 text-transparent bg-clip-text bg-gradient-to-r from-orange-500 via-pink-500 to-blue-700">
               Download resume
@@ -24,8 +22,9 @@ const Resume = () => {
       <div className="flex flex-col items-center py-12">
         <iframe
           className="rounded-lg shadow-image"
-          src="/Kino.pdf"
-          width="1000px"
+          
+          src="/cv.pdf"
+          width="85%"
           height="1000px"
         ></iframe>
       </div>
