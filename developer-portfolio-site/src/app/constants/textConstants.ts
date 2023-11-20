2401bb49d7649b1fd68416947799f0d602a55a93
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-20 17:30:30 +0100

    add alt properties for all projects images

--- developer-portfolio-site/src/app/constants/textConstants.ts
+++ developer-portfolio-site/src/app/constants/textConstants.ts
@@ -76,7 +76,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "React ◆ Redux ◆ Node.js ◆ Express ◆ MongoDB ◆ Material UI ◆ Jest",
     description:
       "Fullstack application with token authentication for login, where users add info on blogs (author, title and link) and can vote for blogs they like. The app also allows users to leave a comment or see how many blogs were added by other users. My first encounter with Material UI as well.",
-    alt: "",
+    alt: "bloglist project desktop screen",
     live: "https://bloglist-frontend-d1wc.onrender.com",
     code: "https://github.com/MateuszKuruc/fullStackOpen-nodeJS",
   },
@@ -86,7 +86,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "React ◆ Node.js ◆ Express ◆ MongoDB",
     description:
       "Very simple fullstack app allowing us to add and delete contacts (name and phone number). In this project I was playing around with responsive filtering and basic validation for contact form. I also learned how to deploy such apps to the internet so they are widely available.",
-    alt: "test",
+    alt: "phonebook project desktop screen",
     live: "https://phonebook-frontend-sfiq.onrender.com",
     code: "https://github.com/MateuszKuruc/fullStackOpen-nodeJS/tree/main/backend-phonebook",
   },
@@ -96,7 +96,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "Your classic to-do list app that each new developer writes at some point. At the time it has taken me long hours to figure out how to handle logic for adding editing and deleting tasks, incorporate a library (date-fns in this case) dynamic sorting of tasks and local storage.",
-    alt: "test",
+    alt: "to do list project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-todo-list/",
     code: "https://github.com/MateuszKuruc/odin-todo-list",
   },
@@ -106,7 +106,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "Another classic - weather app which was a great opportunity for me to use external API for the first time. It taught me a lot about data handling and the ways of formatting and using it to display it to the user in the way I want. Additionally, I learned some basics about designing toggling for websites.",
-    alt: "test",
+    alt: "weather project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-weather-app/",
     code: "https://github.com/MateuszKuruc/odin-weather-app",
   },
@@ -116,7 +116,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "This is a project that was amazing training for building a bit more complex website layout. I went for a sidebar menu and a simple display of my other projects in the main app's container. The focus was on UI, so most of the buttons and texts are just mock or placeholders with little logic.",
-    alt: "test",
+    alt: "admin dashboard project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-admin-dashboard/",
     code: "https://github.com/MateuszKuruc/odin-admin-dashboard",
   },
@@ -126,7 +126,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "This simplified landing page for a lovely confectionery is not only my tribute to sweets (which I love with all my heart), but my first attempt at responsive design. I learned how to provide good experience to user on all screen sizes, which was a real milestone for me.",
-    alt: "test",
+    alt: "restaurant project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-restaurant-page/",
     code: "https://github.com/MateuszKuruc/odin-restaurant-page",
   },
@@ -136,7 +136,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "One of the first projects on my JavaScript learning journey. It gave me some foundations when it comes to conditional and switch statements in programming. This project was a solid training in terms of DOM manipulation, too. As a bonus, I added keyboard support for the app.",
-    alt: "test",
+    alt: "calculator project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-calculator/",
     code: "https://github.com/MateuszKuruc/odin-calculator",
   },
@@ -146,7 +146,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "Library app allows users to add the books to their shelve. It was a chance to learn about object constructors in JavaScript. It might have also been my first attempt at form validation, as each book needs to include author name, book title and number of pages.",
-    alt: "test",
+    alt: "library project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-library/",
     code: "https://github.com/MateuszKuruc/odin-library",
   },
@@ -156,7 +156,7 @@ export const SPAGHETTI_PROJECTS = [
     stack: "HTML ◆ CSS ◆ JavaScript",
     description:
       "Etch a sketch is simply an app for making doodles in the web browser. Choose and change grid size, erase your lines or remove grid to have a plain canvas if that is your preference. I struggled really hard with nested loops and leveraging CSS Flex in this project but it paid off in future projects!",
-    alt: "test",
+    alt: "etch a sketch project desktop screen",
     live: "https://mateuszkuruc.github.io/odin-etch-a-sketch/",
     code: "https://github.com/MateuszKuruc/odin-etch-a-sketch",
   },
